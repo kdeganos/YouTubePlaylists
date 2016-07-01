ae4273224d4a77d08da802ad824d96eae22f3790
Kevin Deganos <kdeganos-09@sandiego.edu>
2016-07-01 09:18:41 -0700

    Add MainActivityTest and pass test of button text.

---
+++ app/src/test/java/com/epicodus/youtubeplaylists/MainActivityTest.java
@@ -0,0 +1,34 @@
+package com.epicodus.youtubeplaylists;
+
+import android.os.Build;
+import android.widget.Button;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by Guest on 7/1/16.
+ */
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateButtonText() {
+        Button signInButton = (Button) activity.findViewById(R.id.signInButton);
+        assertTrue("Sign In".equals(signInButton.getText().toString()));
+    }
+}
